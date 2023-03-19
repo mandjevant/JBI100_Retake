c5db476c1542940965a16488670b93c979d9bd09
mandjevant <38689620+mandjevant@users.noreply.github.com>
2023-03-19 11:06:52 +0100

    framework

---
+++ dashframework-main/jbi100_app/data/load_data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#replace <path on you computer to file, e.g. C:\....> with the path to the file 
+df = pd.read_csv(r'<path on you computer to file, e.g. C:\....>\tic_data.csv')  
+print(df)
